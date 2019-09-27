a09d33315f78a6cc70f4177583056d76cf4bd4e8
Daniel <daniel-lutz@live.at>
2019-09-27 22:55:31 +0200

    Fix cms previewTemplate, move topic into right folder

--- src/cms/previewTemplates/TopicPreviewTemplate.tsx
+++ src/cms/previewTemplates/TopicPreviewTemplate.tsx
@@ -1,9 +1,6 @@
-import * as React from 'react'
-import { CloudinaryContext } from 'cloudinary-react'
+import React from 'react'
 
 import { TopicTemplate } from '../../templates/TopicTemplate'
-
-const { GATSBY_CLOUDINARY_CLOUD_NAME } = process.env
 
 const TopicPreviewTemplate = ({
   entry,
@@ -12,13 +9,11 @@ const TopicPreviewTemplate = ({
   getAsset,*/,
 }: NetlifyCMSPreviewTemplateProps) => {
   return (
-    <CloudinaryContext cloudName={GATSBY_CLOUDINARY_CLOUD_NAME}>
-      <TopicTemplate
-        title={entry.getIn(['data', 'title'])}
-        description={entry.getIn(['data', 'description'])}
-        html={widgetFor('body')}
-      />
-    </CloudinaryContext>
+    <TopicTemplate
+      title={entry.getIn(['data', 'title'])}
+      description={entry.getIn(['data', 'description'])}
+      html={widgetFor('body')}
+    />
   )
 }
 
